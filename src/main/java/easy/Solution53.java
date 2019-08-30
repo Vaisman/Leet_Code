14d53a1ea7050f672f71897b25acd5fa2f684492
Vasili Svirski <vasili.svirski@gmail.com>
2019-08-30 18:57:19 +0300

    Max subarray sum

---
+++ src/main/java/easy/Solution53.java
@@ -0,0 +1,20 @@
+package easy;
+
+public class Solution53 {
+    public int maxSubArray(int[] nums) {
+        if (nums ==null || nums.length == 0)
+            return 0;
+
+        int maxSum = nums[0];
+        int sum = nums[0];
+        for(int i = 1; i< nums.length; i++) {
+            sum = Math.max(sum, nums[i] + sum);
+            maxSum = Math.max(sum, maxSum);
+        }
+        return maxSum;
+    }
+}
+/*
+Input: [-2,1,-3,4,-1,2,1,-5,4],
+Output: 6
+*/
