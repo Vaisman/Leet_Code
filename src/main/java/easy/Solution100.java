85edd49662aa1759ff0924a639775416b3079099
Vasili Svirski <vasili.svirski@gmail.com>
2019-09-06 18:42:45 +0300

    Same Tree

---
+++ src/main/java/easy/Solution100.java
@@ -0,0 +1,14 @@
+package easy;
+
+import common.TreeNode;
+
+public class Solution100 {
+    public boolean isSameTree(TreeNode p, TreeNode q) {
+        if (p == null || q == null)
+            return p == q;
+
+        return p.val == q.val &&
+                isSameTree(p.left, q.left) &&
+                isSameTree(p.right, q.right);
+    }
+}
