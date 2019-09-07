c30177e79c9e371d387c9563abe847e04a5d09fa
Vasili Svirski <vasili.svirski@gmail.com>
2019-09-07 23:19:05 +0300

    Single number

---
+++ src/main/java/easy/Solution136.java
@@ -0,0 +1,17 @@
+package easy;
+
+public class Solution136 {
+    public int singleNumber(int[] nums) {
+        if (nums == null || nums.length == 0)
+            return 0;
+
+        if (nums.length == 1)
+            return nums[0];
+
+        int res = nums[0];
+        for(int i = 1; i< nums.length; i++) {
+            res ^= nums[i];
+        }
+        return res;
+    }
+}
