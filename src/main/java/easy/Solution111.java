369fcbc8a65de76acfc3fe1b05b86ec0b3e467a2
Vasili Svirski <vasili.svirski@gmail.com>
2019-09-07 11:16:46 +0300

    Min Depth

---
+++ src/main/java/easy/Solution111.java
@@ -0,0 +1,12 @@
+package easy;
+
+import common.TreeNode;
+
+public class Solution111 {
+    public int minDepth(TreeNode root) {
+        if(root == null) return 0;
+        int left = minDepth(root.left);
+        int right = minDepth(root.right);
+        return (left == 0 || right == 0) ? left + right + 1: Math.min(left, right) + 1;
+    }
+}
