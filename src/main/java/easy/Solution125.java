d4abd2412523695bf7c9b5212c858be2161a074a
Vasili Svirski <vasili.svirski@gmail.com>
2019-09-07 22:26:00 +0300

    Valid palindrome

---
+++ src/main/java/easy/Solution125.java
@@ -0,0 +1,23 @@
+package easy;
+
+public class Solution125 {
+    public boolean isPalindrome(String s) {
+        if (s == null || s.length() == 0)
+            return true;
+
+        int l = 0;
+        int r = s.length()-1;
+
+        while (l<r) {
+            if (Character.isLetterOrDigit(s.charAt(l))) { l++; continue;}
+            if (Character.isLetterOrDigit(s.charAt(r))) { r--; continue;}
+
+            if (Character.toLowerCase(s.charAt(l)) != Character.toLowerCase(s.charAt(r))) {
+                return false;
+            }
+            l++;
+            r--;
+        }
+        return true;
+    }
+}
