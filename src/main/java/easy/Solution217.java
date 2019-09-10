5ef40ac33c994dc391c340bb4f865920777aef7f
Vasili Svirski <vasili.svirski@gmail.com>
2019-09-10 17:06:04 +0300

    Contains duplicate

---
+++ src/main/java/easy/Solution217.java
@@ -0,0 +1,19 @@
+package easy;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class Solution217 {
+    public boolean containsDuplicate(int[] nums) {
+        if (nums == null || nums.length <= 1)
+            return true;
+
+        Set<Integer> mem = new HashSet<>();
+        for (int i: nums) {
+            if (mem.contains(i))
+                return true;
+            mem.add(i);
+        }
+        return false;
+    }
+}
