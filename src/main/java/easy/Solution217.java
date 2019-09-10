011939c4fb7a4ed0591f5037b04d0604b1ae16d2
Vasili Svirski <vasili.svirski@gmail.com>
2019-09-10 17:11:15 +0300

    Contains nearby duplicates

--- src/main/java/easy/Solution217.java
+++ src/main/java/easy/Solution217.java
@@ -6,7 +6,7 @@ import java.util.Set;
 public class Solution217 {
     public boolean containsDuplicate(int[] nums) {
         if (nums == null || nums.length <= 1)
-            return true;
+            return false;
 
         Set<Integer> mem = new HashSet<>();
         for (int i: nums) {
