bdd997aec4dac088372b9fc7840bcfefbfe603a4
Vasili Svirski <vasili.svirski@gmail.com>
2019-09-11 18:15:20 +0300

    Word pattern

---
+++ src/main/java/easy/Solution290.java
@@ -0,0 +1,32 @@
+package easy;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Objects;
+
+public class Solution290 {
+    public boolean wordPattern(String pattern, String str) {
+        if (pattern == null || pattern.length() == 0 || str == null || str.length() == 0) {
+            return true;
+        }
+
+        Map<String, Integer> map1 = new HashMap<>();
+        Map<String, Integer> map2 = new HashMap<>();
+
+        String[] splitted = str.split(" ");
+        if (splitted.length != pattern.length()) {
+            return false;
+        }
+
+        for (int i = 0; i < pattern.length(); i++) {
+            if (!Objects.equals(map1.put(String.valueOf(pattern.charAt(i)), i), map2.put(splitted[i], i)))
+                return false;
+        }
+        return true;
+    }
+}
+
+/*
+Input: pattern = "abba", str = "dog cat cat dog"
+Output: true
+*/
