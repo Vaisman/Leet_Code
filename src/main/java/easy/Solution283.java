04be6600fc0387ba63c0148b10ba1dd16a0f7fc2
Vasili Svirski <vasili.svirski@gmail.com>
2019-09-11 16:07:23 +0300

    Move zeros

---
+++ src/main/java/easy/Solution283.java
@@ -0,0 +1,21 @@
+package easy;
+
+public class Solution283 {
+    public void moveZeroes(int[] nums) {
+        if (nums.length == 0) {
+            return;
+        }
+
+        int j = 0;
+        for(int i =0; i < nums.length; i++){
+            if (nums[i] != 0) {
+                nums[j] = nums[i];
+                j++;
+            }
+        }
+
+        for(; j < nums.length; j++){
+            nums[j] = 0;
+        }
+    }
+}
