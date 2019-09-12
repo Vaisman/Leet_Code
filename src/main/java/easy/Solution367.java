5e7d0a990ab657c01c8d2f31f19d0d6da721d63e
Vasili Svirski <vasili.svirski@gmail.com>
2019-09-12 14:23:39 +0300

    Is perfect square

---
+++ src/main/java/easy/Solution367.java
@@ -0,0 +1,20 @@
+package easy;
+
+public class Solution367 {
+    public boolean isPerfectSquare(int num) {
+        int low = 1;
+        int high = num;
+
+        while (low <= high) {
+            long mid = (low + high) / 2;
+            if (mid * mid == num) {
+                return true;
+            } else if (mid * mid < num) {
+                low = (int) mid + 1;
+            } else {
+                high = (int) mid - 1;
+            }
+        }
+        return false;
+    }
+}
