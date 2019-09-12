43b3d269efd34867473305864fe488d0cecc062c
Vasili Svirski <vasili.svirski@gmail.com>
2019-09-12 14:51:03 +0300

    Sum of Two Integers

---
+++ src/main/java/easy/Solution371.java
@@ -0,0 +1,11 @@
+package easy;
+
+public class Solution371 {
+    public int getSum(int a, int b) {
+        if (b == 0)
+            return a;
+        int sum = a ^ b;
+        int carry = (a & b) << 1;
+        return getSum(sum, carry);
+    }
+}
