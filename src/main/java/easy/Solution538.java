38cbce50ae2af929b656e9ad89309c6177fa138f
Vasili Svirski <vasili.svirski@gmail.com>
2019-09-17 18:05:38 +0300

    Convert BST

---
+++ src/main/java/easy/Solution538.java
@@ -0,0 +1,19 @@
+package easy;
+
+import common.TreeNode;
+
+public class Solution538 {
+    int sum = 0;
+
+    public TreeNode convertBST(TreeNode root) {
+        if (root == null) return null;
+
+        convertBST(root.right);
+
+        root.val += sum;
+        sum = root.val;
+
+        convertBST(root.left);
+        return root;
+    }
+}
