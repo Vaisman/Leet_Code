75d6d567ab241a60911e0d5fc7262d6b5414edee
Vasili Svirski <vasili.svirski@gmail.com>
2019-09-17 18:24:59 +0300

    Convert BST

--- src/main/java/easy/Solution538.java
+++ src/main/java/easy/Solution538.java
@@ -1,5 +1,7 @@
 package easy;
 
+import java.util.Stack;
+
 import common.TreeNode;
 
 public class Solution538 {
@@ -14,6 +16,26 @@ public class Solution538 {
         sum = root.val;
 
         convertBST(root.left);
+        return root;
+    }
+
+    public TreeNode convertBST1(TreeNode root) {
+        if (root == null) return null;
+        int sum = 0;
+
+        Stack<TreeNode> stack = new Stack<>();
+        TreeNode cur = root;
+        while (!stack.isEmpty() || cur != null) {
+            while (cur != null) {
+                stack.push(cur);
+                cur = cur.right;
+            }
+            cur = stack.pop();
+            int tmp = cur.val;
+            cur.val += sum;
+            sum += tmp;
+            cur = cur.left;
+        }
         return root;
     }
 }
