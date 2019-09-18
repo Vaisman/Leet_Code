c2f1108ab1b48c86cc1fabfb07ec8ca2cafc2819
Vasili Svirski <vasili.svirski@gmail.com>
2019-09-18 17:55:01 +0300

    Array pair sum

---
+++ src/main/java/easy/Solution561.java
@@ -0,0 +1,18 @@
+package easy;
+
+import java.util.Arrays;
+
+public class Solution561 {
+    public int arrayPairSum(int[] nums) {
+        if (nums == null || nums.length == 0) {
+            return 0;
+        }
+
+        Arrays.sort(nums);
+        int sum = 0;
+        for(int i =0; i < nums.length; i+=2) {
+            sum += nums[i];
+        }
+        return sum;
+    }
+}
