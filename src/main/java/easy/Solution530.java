55da7751639b8a21db8a2f74a7b4c26fea877e4e
Vasili Svirski <vasili.svirski@gmail.com>
2019-09-20 14:14:28 +0300

    Get minimum difference

--- src/main/java/easy/Solution530.java
+++ src/main/java/easy/Solution530.java
@@ -1,24 +1,51 @@
 package easy;
 
+import org.junit.Test;
+
 import common.TreeNode;
 
 public class Solution530 {
+
+    long minDiff = Long.MAX_VALUE;
+
     public int getMinimumDifference(TreeNode root) {
-        if (root == null) return Integer.MAX_VALUE;
-        traverse(root);
-        return diff;
+        helper(root,Long.MIN_VALUE,Long.MAX_VALUE);
+        return (int)minDiff;
     }
 
-    int diff = Integer.MAX_VALUE;
-    TreeNode prev = null;
-    private void traverse(TreeNode root) {
-        if (root == null) return;
+    private void helper(TreeNode curr, long lb, long rb){
+        if(curr==null) return;
+        if(lb!=Long.MIN_VALUE){
+            minDiff = Math.min(minDiff,curr.val - lb);
+        }
+        if(rb!=Long.MAX_VALUE){
+            minDiff = Math.min(minDiff,rb - curr.val);
+        }
+        helper(curr.left,lb,curr.val);
+        helper(curr.right,curr.val,rb);
+    }
 
-        traverse(root.left);
-        if (prev != null)
-            diff = Math.min(diff, Math.abs(prev.val - root.val));
 
-        prev = root;
-        traverse(root.right);
+    @Test
+    public void test() {
+        TreeNode node = new TreeNode(236);
+        node.left = new TreeNode(104);
+        node.right = new TreeNode(701);
+        node.left.right = new TreeNode(227);
+        node.left.right.right = new TreeNode(229);
+        node.left.right.right.right = new TreeNode(233);
+        node.left.right.right.left = new TreeNode(228);
+
+        node.right.right = new TreeNode(911);
+
+        getMinimumDifference(node);
     }
 }
+
+/*
+           236,
+     104,       701,
+  null, 227,  null, 911
+           229
+         228  233
+ */
