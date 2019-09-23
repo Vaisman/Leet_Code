460368437bc7d2fe6cedf8527a9dfe814009cb36
Vasili Svirski <vasili.svirski@gmail.com>
2019-09-23 23:03:45 +0300

    Min distance BST

---
+++ src/main/java/easy/Solution783.java
@@ -0,0 +1,15 @@
+package easy;
+
+import common.TreeNode;
+
+public class Solution783 {
+    int min = Integer.MAX_VALUE;
+    Integer res = Integer.MAX_VALUE, pre = null;
+    public int minDiffInBST(TreeNode root) {
+        if (root.left != null) minDiffInBST(root.left);
+        if (pre != null) res = Math.min(res, root.val - pre);
+        pre = root.val;
+        if (root.right != null) minDiffInBST(root.right);
+        return res;
+    }
+}
