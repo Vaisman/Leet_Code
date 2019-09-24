59c2f1d467057d6d54dfcdfaf7e96930852926e2
Vasili Svirski <vasili.svirski@gmail.com>
2019-09-24 18:32:16 +0300

    Backspace compare

---
+++ src/main/java/easy/Solution844.java
@@ -0,0 +1,20 @@
+package easy;
+
+import java.util.Stack;
+
+public class Solution844 {
+    public boolean backspaceCompare(String S, String T) {
+        return build(S).equals(build(T));
+    }
+
+    public String build(String S) {
+        Stack<Character> ans = new Stack();
+        for (char c: S.toCharArray()) {
+            if (c != '#')
+                ans.push(c);
+            else if (!ans.empty())
+                ans.pop();
+        }
+        return String.valueOf(ans);
+    }
+}
