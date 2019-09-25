aafe4f08fe6ac5df3ed491559269c4f2bf97f52b
Vasili Svirski <vasili.svirski@gmail.com>
2019-09-25 10:50:19 +0300

    Is monotonic

---
+++ src/main/java/easy/Solution986.java
@@ -0,0 +1,19 @@
+package easy;
+
+public class Solution986 {
+    public boolean isMonotonic(int[] A) {
+        return increasing(A) || decreasing(A);
+    }
+
+    private boolean increasing(int[] A) {
+        for (int i = 0; i < A.length - 1; ++i)
+            if (A[i] > A[i+1]) return false;
+        return true;
+    }
+
+    private boolean decreasing(int[] A) {
+        for (int i = 0; i < A.length - 1; ++i)
+            if (A[i] < A[i+1]) return false;
+        return true;
+    }
+}
