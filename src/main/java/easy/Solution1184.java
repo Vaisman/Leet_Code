53e0a4ec17a5f0485867cfff0e5f52592d93051e
Vasili Svirski <vasili.svirski@gmail.com>
2019-09-26 13:16:40 +0300

    Distance between bus stops

---
+++ src/main/java/easy/Solution1184.java
@@ -0,0 +1,22 @@
+package easy;
+
+public class Solution1184 {
+    public int distanceBetweenBusStops(int[] distance, int start, int destination) {
+        if (start > destination) {
+            int temp = start;
+            start = destination;
+            destination = temp;
+        }
+
+        int res = 0;
+        int total = 0;
+        for (int i = 0; i < distance.length; i++) {
+            if (i >= start && i < destination) {
+                res += distance[i];
+            }
+            total += distance[i];
+        }
+
+        return Math.min(res, total - res);
+    }
+}
