48bb435cbe793196e5824ef05a68a9385e0f0d5e
Vasili Svirski <vasili.svirski@gmail.com>
2019-09-27 20:46:16 +0300

    Remove duplicates

---
+++ src/main/java/easy/Solution1047.java
@@ -0,0 +1,22 @@
+package easy;
+
+import java.util.Stack;
+
+public class Solution1047 {
+    public String removeDuplicates(String S) {
+        Stack<Character> stack = new Stack<>();
+
+        for(char s : S.toCharArray()){
+            if(!stack.isEmpty() && stack.peek() == s)
+                stack.pop();
+            else
+                stack.push(s);
+        }
+
+        StringBuilder sb = new StringBuilder();
+        for(char s : stack) {
+            sb.append(s);
+        }
+        return sb.toString();
+    }
+}
