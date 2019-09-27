11e81ef32ca3947145ef3aded818eedd363e9c38
Vasili Svirski <vasili.svirski@gmail.com>
2019-09-27 20:28:10 +0300

    Duplicate zeros

---
+++ src/main/java/easy/Solution1089.java
@@ -0,0 +1,31 @@
+package easy;
+
+import org.junit.Test;
+
+public class Solution1089 {
+    public void duplicateZeros(int[] arr) {
+        int countZero = 0;
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] == 0) countZero++;
+        }
+
+        int len = arr.length + countZero;
+        //We just need O(1) space if we scan from back
+        //i point to the original array, j point to the new location
+        for (int i = arr.length - 1, j = len - 1; i < j; i--, j--) {
+            if (arr[i] != 0) {
+                if (j < arr.length) arr[j] = arr[i];
+            } else {
+                if (j < arr.length) arr[j] = arr[i];
+                j--;
+                if (j < arr.length) arr[j] = arr[i]; //copy twice when hit '0'
+            }
+        }
+    }
+
+    @Test
+    public void test() {
+        int[] list = new int[]{1,0,2,3,0,4,5,0};
+        duplicateZeros(list);
+    }
+}
