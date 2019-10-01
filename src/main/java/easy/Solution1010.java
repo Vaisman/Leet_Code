0440ca3574e5af515c985f4a14e4ac8737ff8f7e
Vasili Svirski <vasili.svirski@gmail.com>
2019-10-01 18:12:33 +0300

    1010 Follow up, array

--- src/main/java/easy/Solution1010.java
+++ src/main/java/easy/Solution1010.java
@@ -21,6 +21,26 @@ public class Solution1010 {
         return ans;
     }
 
+    /*
+    public int numPairsDivisibleBy60(int[] time) {
+        int[] count=new int[60];
+        for(int num: time){
+            count[num%60]++;
+        }
+
+        int res = 0;
+        for(int i=0;i<=30;i++){
+            if(i==0||i==30){
+                res += count[i]*(count[i]-1)/2;
+            } else{
+
+            res+=count[i]*count[60-i];
+            }
+        }
+        return res;
+    }
+     */
+
     @Test
     public void test() {
         assertEquals(numPairsDivisibleBy60(new int[]{30,20,150,100,40}), 3 );
