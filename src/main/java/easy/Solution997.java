1b720a3b1d20ca23846fbd0be10e81a8a50b1529
Vasili Svirski <vasili.svirski@gmail.com>
2019-10-02 10:52:54 +0300

    Find judge

---
+++ src/main/java/easy/Solution997.java
@@ -0,0 +1,28 @@
+package easy;
+
+public class Solution997 {
+    public int findJudge(int N, int[][] trust) {
+        if (trust == null || trust.length == 0 || trust[0].length == 0) {
+            return -1;
+        }
+
+        int[] count = new int[N+1];
+        for (int[] t: trust) {
+            count[t[0]]--;
+            count[t[1]]++;
+        }
+
+        for (int i = 1; i <= N; i++) {
+            if (count[i] == N - 1) {
+                return i;
+            }
+        }
+
+        return -1;
+    }
+}
+
+/*
+trust as a graph, all pairs are directed edge.
+The point with in-degree - out-degree = N - 1
+*/
