1f28118c9866c4199838537a46445106d4539c06
Vasili Svirski <vasili.svirski@gmail.com>
2019-10-08 16:36:52 +0300

    Letter combinations

---
+++ src/main/java/medium/Solution17.java
@@ -0,0 +1,30 @@
+package medium;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+
+public class Solution17 {
+    private static final String[] KEYS = { "", "", "abc", "def", "ghi", "jkl", "mno", "pqrs", "tuv", "wxyz" };
+
+    public List<String> letterCombinations(String digits) {
+        if (digits == null || digits.length() == 0) {
+            return new ArrayList<>();
+
+        }
+        List<String> ret = new LinkedList<>();
+        combination("", digits, 0, ret);
+        return ret;
+    }
+
+    private void combination(String prefix, String digits, int offset, List<String> ret) {
+        if (offset >= digits.length()) {
+            ret.add(prefix);
+            return;
+        }
+        String letters = KEYS[(digits.charAt(offset) - '0')];
+        for (int i = 0; i < letters.length(); i++) {
+            combination(prefix + letters.charAt(i), digits, offset + 1, ret);
+        }
+    }
+}
