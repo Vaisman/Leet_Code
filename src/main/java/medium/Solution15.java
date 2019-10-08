ee1ad3ad1fef04ef49bf658dcc7751b19dcd0782
Vasili Svirski <vasili.svirski@gmail.com>
2019-10-08 15:39:31 +0300

    Three sum

---
+++ src/main/java/medium/Solution15.java
@@ -0,0 +1,32 @@
+package medium;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.LinkedList;
+import java.util.List;
+
+public class Solution15 {
+    public List<List<Integer>> threeSum(int[] num) {
+        if (num == null || num.length ==0) {
+            return new ArrayList<>();
+        }
+
+        Arrays.sort(num);
+        List<List<Integer>> res = new LinkedList<>();
+        for (int i = 0; i < num.length-2; i++) {
+            if (i == 0 || (i > 0 && num[i] != num[i-1])) {
+                int lo = i+1, hi = num.length-1, sum = 0 - num[i];
+                while (lo < hi) {
+                    if (num[lo] + num[hi] == sum) {
+                        res.add(Arrays.asList(num[i], num[lo], num[hi]));
+                        while (lo < hi && num[lo] == num[lo+1]) lo++;
+                        while (lo < hi && num[hi] == num[hi-1]) hi--;
+                        lo++; hi--;
+                    } else if (num[lo] + num[hi] < sum) lo++;
+                    else hi--;
+                }
+            }
+        }
+        return res;
+    }
+}
