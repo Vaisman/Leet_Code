6dca008b8ecf318b11b1b4a8f1c16d4c60d9dc4e
Vasili Svirski <vasili.svirski@gmail.com>
2019-11-01 12:42:23 +0300

    Longest substring

---
+++ src/main/java/medium/Solution395.java
@@ -0,0 +1,23 @@
+package medium;
+
+public class Solution395 {
+    public int longestSubstring(String s, int k) {
+        return helper(s.toCharArray(), 0, s.length(), k);
+    }
+
+    public int helper(char[] ch, int l, int r, int k) {
+        if (r - l < k)
+            return 0;
+
+        int[] map = new int[26];
+        for (int i = l; i < r; i++)
+            map[ch[i] - 'a']++;
+
+        for (int i = l; i < r; i++) {
+            if (map[ch[i] - 'a'] < k) {
+                return Math.max(helper(ch, l, i, k), helper(ch, i+1, r, k));
+            }
+        }
+        return r - l;
+    }
+}
