52976472a447edb1ef0d631e9a1e05264dbe7706
Vasili Svirski <vasili.svirski@gmail.com>
2019-11-13 18:48:16 +0300

    Split string on balanced strings

---
+++ src/main/java/easy/Solution1221.java
@@ -0,0 +1,12 @@
+package easy;
+
+public class Solution1221 {
+    public int balancedStringSplit(String s) {
+        int res = 0, cnt = 0;
+        for (int i = 0; i < s.length(); ++i) {
+            cnt += s.charAt(i) == 'L' ? 1 : -1;
+            if (cnt == 0) ++res;
+        }
+        return res;
+    }
+}
