eaa15bb03896cb6a15aadd639ea3bf51bca6207f
Vasili Svirski <vasili.svirski@gmail.com>
2019-11-14 16:14:26 +0300

    Max envelopes

---
+++ src/main/java/hard/Solution354.java
@@ -0,0 +1,34 @@
+package hard;
+
+import java.util.Arrays;
+
+public class Solution354 {
+    private int lengthOfLIS(int[] nums) {
+        int[] dp = new int[nums.length];
+        int len = 0;
+        for (int num : nums) {
+            int i = Arrays.binarySearch(dp, 0, len, num);
+            if (i < 0) {
+                i = -(i + 1);
+            }
+            dp[i] = num;
+            if (i == len) {
+                len++;
+            }
+        }
+        return len;
+    }
+
+    public int maxEnvelopes(int[][] envelopes) {
+        Arrays.sort(envelopes, (a, b) -> {
+                if (a[0] == b[0]) {
+                    return b[1] - a[1];
+                } else {
+                    return a[0] - b[0];
+                }
+        });
+        int[] secondDim = new int[envelopes.length];
+        for (int i = 0; i < envelopes.length; ++i) secondDim[i] = envelopes[i][1];
+        return lengthOfLIS(secondDim);
+    }
+}
