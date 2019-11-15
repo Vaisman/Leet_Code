d97d114c7feca5c330707364b84b1393e1173a08
Vasili Svirski <vasili.svirski@gmail.com>
2019-11-15 15:16:53 +0300

    Get max gold, BFS

--- src/main/java/medium/Solution1219.java
+++ src/main/java/medium/Solution1219.java
@@ -1,5 +1,8 @@
 package medium;
 
+import java.util.LinkedList;
+import java.util.Queue;
+
 public class Solution1219 {
     public int getMaximumGold(int[][] grid) {
         int ans = 0;
@@ -23,5 +26,36 @@ public class Solution1219 {
         }
         g[i][j] -= 1000;
         return mx;
+    }
+
+    public int getMaximumGold1(int[][] grid) {
+        int ans = 0, m = grid.length, n = grid[0].length;
+        int[][] oneCellTrace = new int[m][n];
+        Queue<int[]> q = new LinkedList<>();
+        for (int i = 0, goldCellId = 0; i < m; ++i) {
+            for (int j = 0; j < n; ++j) {
+                if (grid[i][j] > 0) {
+                    oneCellTrace[i][j] = 1 << goldCellId++;
+                    q.offer(new int[]{i, j, grid[i][j], oneCellTrace[i][j]});
+                }
+            }
+        }
+
+        while (!q.isEmpty()) {
+            int i = q.peek()[0];
+            int j = q.peek()[1];
+            int sum = q.peek()[2];
+            int trace = q.poll()[3];
+            ans = Math.max(sum, ans);
+
+            for (int k = 0; k < 4; ++k) {
+                int r = i + d[k], c = j + d[k + 1];
+                if (r >= 0 && r < m && c >= 0 && c < n && grid[r][c] > 0
+                        && (trace & oneCellTrace[r][c]) == 0) {
+                    q.offer(new int[]{r, c, sum + grid[r][c], trace | oneCellTrace[r][c]});
+                }
+            }
+        }
+        return ans;
     }
 }
