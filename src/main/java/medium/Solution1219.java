89ffccd730cbcb42210338d7429ea31b403aea3c
Vasili Svirski <vasili.svirski@gmail.com>
2019-11-15 14:12:47 +0300

    Get max gold, DFS

---
+++ src/main/java/medium/Solution1219.java
@@ -0,0 +1,27 @@
+package medium;
+
+public class Solution1219 {
+    public int getMaximumGold(int[][] grid) {
+        int ans = 0;
+        for (int i = 0; i < grid.length; i++) {
+            for (int j = 0; j < grid[0].length; j++) {
+                ans = Math.max(ans, dfs(grid, i, j, 0));
+            }
+        }
+        return ans;
+    }
+
+    private static final int[] d = {0, 1, 0, -1, 0};
+    private int dfs(int[][] g, int i, int j, int sum) {
+        if (i < 0 || i >= g.length || j < 0 || j >= g[0].length || g[i][j] == 0 || g[i][j] > 100)
+            return sum;
+        sum += g[i][j];
+        g[i][j] += 1000;
+        int mx = 0;
+        for (int k = 0; k < 4; k++) {
+            mx = Math.max(mx, dfs(g, i + d[k], j + d[k + 1], sum));
+        }
+        g[i][j] -= 1000;
+        return mx;
+    }
+}
