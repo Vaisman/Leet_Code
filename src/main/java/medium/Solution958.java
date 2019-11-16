a63f6ed4ea9f64a7ec6b9c8a04999ae13d7e1ca8
Vasili Svirski <vasili.svirski@gmail.com>
2019-11-16 13:30:44 +0300

    Is complete tree

---
+++ src/main/java/medium/Solution958.java
@@ -0,0 +1,34 @@
+package medium;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import common.TreeNode;
+
+public class Solution958 {
+
+    public boolean isCompleteTree(TreeNode root) {
+        List<ANode> nodes = new ArrayList();
+        nodes.add(new ANode(root, 1));
+        int i = 0;
+        while (i < nodes.size()) {
+            ANode anode = nodes.get(i++);
+            if (anode.node != null) {
+                nodes.add(new ANode(anode.node.left, anode.code * 2));
+                nodes.add(new ANode(anode.node.right, anode.code * 2 + 1));
+            }
+        }
+
+        return nodes.get(i-1).code == nodes.size();
+    }
+}
+
+class ANode {  // Annotated Node
+    TreeNode node;
+    int code;
+    ANode(TreeNode node, int code) {
+        this.node = node;
+        this.code = code;
+    }
+}
+
