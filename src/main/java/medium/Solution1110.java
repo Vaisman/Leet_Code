b5beed3f879e3f0c62fbc152b6821a01593ef04f
Vasili Svirski <vasili.svirski@gmail.com>
2019-12-12 18:33:17 +0300

    Delete nodes

---
+++ src/main/java/medium/Solution1110.java
@@ -0,0 +1,35 @@
+package medium;
+
+import common.TreeNode;
+
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+public class Solution1110 {
+    Set<Integer> to_delete_set;
+    List<TreeNode> res;
+
+    public List<TreeNode> delNodes(TreeNode root, int[] to_delete) {
+        to_delete_set = new HashSet<>();
+        res = new ArrayList<>();
+        for (int i : to_delete)
+            to_delete_set.add(i);
+        helper(root, true);
+        return res;
+    }
+
+    private TreeNode helper(TreeNode node, boolean is_root) {
+        if (node == null) return null;
+        boolean deleted = to_delete_set.contains(node.val);
+        if (is_root && !deleted) {
+            res.add(node);
+        }
+
+        node.left = helper(node.left, deleted);
+        node.right =  helper(node.right, deleted);
+
+        return deleted ? null : node;
+    }
+}
