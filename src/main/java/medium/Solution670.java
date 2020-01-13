c20afe85b2cb033e50d31a46907c691bcdc9fbb1
Vasili Svirski <vasili.svirski@gmail.com>
2020-01-13 16:50:42 +0300

    Maximum swap

---
+++ src/main/java/medium/Solution670.java
@@ -0,0 +1,32 @@
+package medium;
+
+import org.junit.Test;
+
+public class Solution670 {
+    public int maximumSwap(int num) {
+        char[] digits = Integer.toString(num).toCharArray();
+
+        int[] buckets = new int[10];
+        for (int i = 0; i < digits.length; i++) {
+            buckets[digits[i] - '0'] = i;
+        }
+
+        for (int i = 0; i < digits.length; i++) {
+            for (int k = 9; k > digits[i] - '0'; k--) {
+                if (buckets[k] > i) {
+                    char tmp = digits[i];
+                    digits[i] = digits[buckets[k]];
+                    digits[buckets[k]] = tmp;
+                    return Integer.valueOf(new String(digits));
+                }
+            }
+        }
+
+        return num;
+    }
+
+    @Test
+    public void test(){
+       maximumSwap(2736);
+    }
+}
