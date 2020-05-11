807c0d3b3934f7f5df88b1f5bf966bbd53c392f6
Vasili Svirski <vasili.svirski@gmail.com>
2020-05-11 17:08:01 +0300

    Find secret word

---
+++ src/main/java/hard/Solution843.java
@@ -0,0 +1,31 @@
+package hard;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+
+public class Solution843 {
+    interface Master {
+      int guess(String word);
+    }
+
+    public void findSecretWord(String[] wordlist, Master master) {
+        for (int i = 0, x = 0; i < 10 && x < 6; ++i) {
+            String guess = wordlist[new Random().nextInt(wordlist.length)];
+            x = master.guess(guess);
+            List<String> wordlist2 = new ArrayList<>();
+            for (String w : wordlist)
+                if (match(guess, w) == x)
+                    wordlist2.add(w);
+            wordlist = wordlist2.toArray(new String[wordlist2.size()]);
+        }
+    }
+
+    public int match(String a, String b) {
+        int matches = 0;
+        for (int i = 0; i < a.length(); ++i)
+            if (a.charAt(i) == b.charAt(i))
+                matches ++;
+        return matches;
+    }
+}
