c7cc34f337318743e184ebbf13616b99bba1cfb8
Vasili Svirski <vasili.svirski@gmail.com>
2020-05-20 17:01:22 +0300

    Shortest palindrome

---
+++ src/main/java/hard/Solution214.java
@@ -0,0 +1,20 @@
+package hard;
+
+import org.junit.Test;
+
+public class Solution214 {
+    public String shortestPalindrome(String s){
+        int j = 0;
+        for (int i = s.length() - 1; i >= 0; i--) {
+            if (s.charAt(i) == s.charAt(j)) { j += 1; }
+        }
+        if (j == s.length()) { return s; }
+        String suffix = s.substring(j);
+        return new StringBuffer(suffix).reverse().toString() + shortestPalindrome(s.substring(0, j)) + suffix;
+    }
+
+    @Test
+    public void test() {
+        shortestPalindrome("abcd");
+    }
+}
