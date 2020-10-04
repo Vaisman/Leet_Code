d01eed039ccb64bb05919a2be8e5b604f94a069c
Vasili Svirski <vasili.svirski@gmail.com>
2020-10-04 11:27:22 +0300

    Largest item association

--- src/main/java/Amazon/SubstringsOfSizeKwithKDistinctChars.java
+++ src/main/java/Amazon/SubstringsOfSizeKwithKDistinctChars.java
@@ -41,5 +41,33 @@ public class SubstringsOfSizeKwithKDistinctChars {
         uniqueSubstringSizeK("abacab", 3);
         uniqueSubstringSizeK("awaglknagawunagwkwagl", 4);
     }
-
 }
+
+/*
+ public List<String> uniqueSubstringSizeK(String S,int k){
+        int distinct=0,i=0;
+        int [] memo=new int[26];
+        Set<String> set=new HashSet<>();
+        for (;i<k;i++){
+            if (memo[S.charAt(i)-'a']==0)
+                distinct+=1;
+            memo[S.charAt(i)-'a']+=1;
+        }
+        if (distinct==k) {
+            set.add(S.substring(i-k,i));
+        }
+        while (i<S.length()){
+            if (memo[S.charAt(i)-'a']==0)
+                distinct+=1;
+            memo[S.charAt(i)-'a']+=1;
+            memo[S.charAt(i-k)-'a']-=1;
+            if (memo[S.charAt(i-k)-'a']==0)
+                distinct-=1;
+            if (distinct==k)
+                set.add(S.substring(i-k+1,i+1));
+            i+=1;
+            }
+
+        return new ArrayList<>(set);
+    }
+ */
