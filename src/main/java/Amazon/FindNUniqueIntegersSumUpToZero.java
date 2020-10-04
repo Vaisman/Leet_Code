46143f76466fa5e5a62934b13adf3c51f4d2d8e1
Vasili Svirski <vasili.svirski@gmail.com>
2020-10-04 20:50:49 +0300

    Find N unique integers sum up to zero

---
+++ src/main/java/Amazon/FindNUniqueIntegersSumUpToZero.java
@@ -0,0 +1,10 @@
+package Amazon;
+
+public class FindNUniqueIntegersSumUpToZero {
+    public int[] sumZero(int n) {
+        int[] A = new int[n];
+        for (int i = 0; i < n; ++i)
+            A[i] = i * 2 - n + 1;
+        return A;
+    }
+}
