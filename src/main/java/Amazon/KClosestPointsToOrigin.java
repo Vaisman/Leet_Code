36b7b52c05086efabc549b424baf779d0ad135bb
Vasili Svirski <vasili.svirski@gmail.com>
2020-10-06 15:26:44 +0300

    k closest

--- src/main/java/Amazon/KClosestPointsToOrigin.java
+++ src/main/java/Amazon/KClosestPointsToOrigin.java
@@ -3,7 +3,26 @@ package Amazon;
 import java.util.Arrays;
 import java.util.concurrent.ThreadLocalRandom;
 
+// https://leetcode.com/problems/k-closest-points-to-origin/discuss/220235/Java-Three-solutions-to-this-classical-K-th-problem.
+
 public class KClosestPointsToOrigin {
+
+    /*
+    public int[][] kClosest(int[][] points, int K) {
+        PriorityQueue<int[]> pq = new PriorityQueue<int[]>((p1, p2) -> p2[0] * p2[0] + p2[1] * p2[1] - p1[0] * p1[0] - p1[1] * p1[1]);
+        for (int[] p : points) {
+            pq.offer(p);
+            if (pq.size() > K) {
+                pq.poll();
+            }
+        }
+        int[][] res = new int[K][2];
+        while (K > 0) {
+            res[--K] = pq.poll();
+        }
+        return res;
+    }
+     */
     int[][] points;
 
     public int[][] kClosest(int[][] points, int K) {
