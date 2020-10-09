a3e73a384c4b7cab3ad4970b2ee48879daaede0e
Vasili Svirski <vasili.svirski@gmail.com>
2020-10-09 12:56:29 +0300

    Min cost to connect ropes

---
+++ src/main/java/Amazon/MinCostToConnectRopes.java
@@ -0,0 +1,19 @@
+package Amazon;
+
+import java.util.PriorityQueue;
+
+public class MinCostToConnectRopes {
+    public int connectSticks(int[] sticks) {
+        PriorityQueue<Integer> pq = new PriorityQueue<>();
+        for (int s : sticks) {
+            pq.offer(s);
+        }
+        int sum = 0;
+        while (pq.size() > 1) {
+            int two = pq.poll() + pq.poll();
+            sum += two;
+            pq.offer(two);
+        }
+        return sum;
+    }
+}
