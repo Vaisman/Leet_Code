f5b1789b1ea81d42c3ff7b0afb32c0c8cf1a8b2e
Vasili Svirski <vasili.svirski@gmail.com>
2020-10-26 16:06:33 +0300

    Number of Operations to Make Network Connected (DSU, DFS, BFS)

--- src/main/java/medium/Solution1319.java
+++ src/main/java/medium/Solution1319.java
@@ -1,5 +1,10 @@
 package medium;
 
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
+
 public class Solution1319 {
     public int makeConnected(int n, int[][] connections) {
         if (connections.length < n - 1) return -1; // To connect all nodes need at least n-1 edges
@@ -19,5 +24,55 @@ public class Solution1319 {
     private int findParent(int[] parent, int i) {
         while (i != parent[i]) i = parent[i];
         return i; // Without Path Compression
+    }
+
+    public int makeConnected1(int n, int[][] connections) {
+        if (connections.length < n - 1) return -1; // To connect all nodes need at least n-1 edges
+        List<Integer>[] graph = new List[n];
+        for (int i = 0; i < n; i++) graph[i] = new ArrayList<>();
+        for (int[] c : connections) {
+            graph[c[0]].add(c[1]);
+            graph[c[1]].add(c[0]);
+        }
+        int components = 0;
+        boolean[] visited = new boolean[n];
+        for (int v = 0; v < n; v++) components += dfs(v, graph, visited);
+        return components - 1; // Need (components-1) cables to connect components together
+    }
+    int dfs(int u, List<Integer>[] graph, boolean[] visited) {
+        if (visited[u]) return 0;
+        visited[u] = true;
+        for (int v : graph[u]) dfs(v, graph, visited);
+        return 1;
+    }
+
+    public int makeConnected2(int n, int[][] connections) {
+        if (connections.length < n - 1) return -1; // To connect all nodes need at least n-1 edges
+        List<Integer>[] graph = new List[n];
+        for (int i = 0; i < n; i++) graph[i] = new ArrayList<>();
+        for (int[] c : connections) {
+            graph[c[0]].add(c[1]);
+            graph[c[1]].add(c[0]);
+        }
+        int components = 0;
+        boolean[] visited = new boolean[n];
+        for (int v = 0; v < n; v++) components += bfs(v, graph, visited);
+        return components - 1; // Need (components-1) cables to connect components together
+    }
+    int bfs(int src, List<Integer>[] graph, boolean[] visited) {
+        if (visited[src]) return 0;
+        visited[src] = true;
+        Queue<Integer> q = new LinkedList<>();
+        q.offer(src);
+        while (!q.isEmpty()) {
+            int u = q.poll();
+            for (int v : graph[u]) {
+                if (!visited[v]) {
+                    visited[v] = true;
+                    q.offer(v);
+                }
+            }
+        }
+        return 1;
     }
 }
