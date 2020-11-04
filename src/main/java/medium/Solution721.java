d5bf60827b93aa3aad5972dd663bf0f30029e18d
Vasili Svirski <vasili.svirski@gmail.com>
2020-11-04 16:19:30 +0300

    Accounts merge

--- src/main/java/medium/Solution721.java
+++ src/main/java/medium/Solution721.java
@@ -3,45 +3,54 @@ package medium;
 import java.util.*;
 
 public class Solution721 {
-    public List<List<String>> accountsMerge(List<List<String>> accounts) {
-        Map<String, String> emailToName = new HashMap();
-        Map<String, ArrayList<String>> graph = new HashMap();
-        for (List<String> account: accounts) {
-            String name = "";
-            for (String email: account) {
-                if (name == "") {
-                    name = email;
-                    continue;
+    /**
+     * Solution 3: Build graph + DFS
+     *
+     * @params graph: one to many mapping graph that connects all emails with same name. The graph may contain several
+     * separated components
+     * @params emailToName: one to one mapping for finding name by any email belong to same group
+     * */
+    public List<List<String>> accountsMerge3(List<List<String>> accounts) {
+        Map<String, Set<String>> graph = new HashMap<>();
+        Map<String, String> emailToName = new HashMap<>();
+
+        // step 1: build graph that connects all emails have relationships
+        for (List<String> account : accounts) {
+            String name = account.get(0);
+            for (int i = 1; i < account.size(); i++) {
+                graph.putIfAbsent(account.get(i), new HashSet<>());
+                emailToName.put(account.get(i), name);
+                if (i != 1) {
+                    graph.get(account.get(i)).add(account.get(i - 1));
+                    graph.get(account.get(i - 1)).add(account.get(i));
                 }
-                graph.computeIfAbsent(email, x-> new ArrayList<String>()).add(account.get(1));
-                graph.computeIfAbsent(account.get(1), x-> new ArrayList<String>()).add(email);
-                emailToName.put(email, name);
             }
         }
 
-        Set<String> seen = new HashSet();
-        List<List<String>> ans = new ArrayList();
-        for (String email: graph.keySet()) {
-            if (!seen.contains(email)) {
-                seen.add(email);
-                Stack<String> stack = new Stack();
-                stack.push(email);
-                List<String> component = new ArrayList();
-                while (!stack.empty()) {
-                    String node = stack.pop();
-                    component.add(node);
-                    for (String nei: graph.get(node)) {
-                        if (!seen.contains(nei)) {
-                            seen.add(nei);
-                            stack.push(nei);
-                        }
-                    }
-                }
-                Collections.sort(component);
-                component.add(0, emailToName.get(email));
-                ans.add(component);
+        // step 2: DFS traversal to traverse all nodes in every single component and generate each result list individually
+        List<List<String>> result = new ArrayList<>();
+        Set<String> visited = new HashSet<>();
+        for (String email : graph.keySet()) {
+            if (!visited.contains(email)) {
+                visited.add(email);
+                List<String> newList = new ArrayList<>();
+                dfs(newList, graph, visited, email);
+                Collections.sort(newList);
+                newList.add(0, emailToName.get(newList.get(0)));
+                result.add(newList);
             }
         }
-        return ans;
+        return result;
+    }
+
+    public void dfs(List<String> result, Map<String, Set<String>> graph, Set<String> visited, String curPoint) {
+        result.add(curPoint);
+        Set<String> neighbors = graph.get(curPoint);
+        for (String neighbor : neighbors) {
+            if (!visited.contains(neighbor)) {
+                visited.add(neighbor);
+                dfs(result, graph, visited, neighbor);
+            }
+        }
     }
 }
