7a7e990620984c4579f62e192cf909e341d8a232
Vasili Svirski <vasili.svirski@gmail.com>
2020-11-05 12:46:03 +0300

    kth grammar

---
+++ src/main/java/medium/Solution779.java
@@ -0,0 +1,8 @@
+package medium;
+
+public class Solution779 {
+    public int kthGrammar(int N, int K) {
+        if (N == 1) return 0;
+        return (~K & 1) ^ kthGrammar(N-1, (K+1)/2);
+    }
+}
