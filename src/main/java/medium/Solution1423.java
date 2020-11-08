89a367ab1dd7ee0dd34b02a43ceba2277e9d4653
Vasili Svirski <vasili.svirski@gmail.com>
2020-11-08 19:00:16 +0300

    Follow up with BFS/DFS

--- src/main/java/medium/Solution1423.java
+++ src/main/java/medium/Solution1423.java
@@ -19,3 +19,34 @@ public class Solution1423 {
         return totalSum - minWindowSum;
     }
 }
+
+/*
+//call this function with (A,B,index of the current element in B, index of the left element in A , index of the right element in B)
+int getMaxSum(vector<int>& A, vector<int>& K, int x, int left,int right){
+    if(x == K.size()){
+        return 0;
+    }
+    int leftSum = A[left]*K[x] + getMaxSum(A,K,x+1,left+1,right);
+    int rightSum = A[right]*K[x] + getMaxSum(A,K,x+1,left,right-1);
+    int curr = max(leftSum,rightSum);
+    return curr;
+}
+
+int maximizeSum(int[] A, int[] B) {
+        int maxsum = Integer.MIN_VALUE;
+        Queue<int[]> queue = new LinkedList<>();
+        queue.offer(new int[]{0, A.length-1, 0, 0}); // (A start, A end, B Start, val)
+        while(!queue.isEmpty()) {
+            int[] info = queue.poll();
+            if(info[2] == B.length) {
+                maxsum = Math.max(maxsum, info[3]);
+            } else {
+                queue.offer(new int[]{info[0]+1, info[1],
+                    info[2]+1, info[3] +  (A[info[0]] * B[info[2]])});
+                queue.offer(new int[]{info[0], info[1]-1,
+                    info[2]+1, info[3] +  (A[info[1]] * B[info[2]])});
+            }
+        }
+        return maxsum;
+    }
+ */
