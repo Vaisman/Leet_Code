08306970fbe8f82a246ef604f8761ff8b5b4924b
Vasili Svirski <vasili.svirski@gmail.com>
2020-11-10 14:52:45 +0300

    Remove duplicate letters

--- src/main/java/medium/Soluition1081.java
+++ src/main/java/medium/Soluition1081.java
@@ -34,22 +34,22 @@ public class Soluition1081 {
     }
 
     public String removeDuplicateLetters(String s) {
+        // find pos - the index of the leftmost letter in our solution
+        // we create a counter and end the iteration once the suffix doesn't have each unique character
+        // pos will be the index of the smallest character we encounter before the iteration ends
         int[] cnt = new int[26];
-        for (int i = 0; i < s.length(); i++) {
-            cnt[s.charAt(i) - 'a']++;
-        }
+        int pos = 0;
+        for (int i = 0; i < s.length(); i++) cnt[s.charAt(i) - 'a']++;
 
-        int pos = 0; // the position for the smallest s[i]
         for (int i = 0; i < s.length(); i++) {
-            if (s.charAt(i) < s.charAt(pos)) {
+            if (s.charAt(i) < s.charAt(pos))
                 pos = i;
-            }
-            if (--cnt[s.charAt(i) - 'a'] == 0) {
+            if (--cnt[s.charAt(i) - 'a'] == 0)
                 break;
-            }
         }
+        // our answer is the leftmost letter plus the recursive call on the remainder of the string
+        // note that we have to get rid of further occurrences of s[pos] to ensure that there are no duplicates
         return s.length() == 0 ? "" : s.charAt(pos) +
-                removeDuplicateLetters(s.substring(pos + 1)
-                        .replaceAll("" + s.charAt(pos), ""));
+                removeDuplicateLetters(s.substring(pos + 1).replaceAll("" + s.charAt(pos), ""));
     }
 }
