70ba07527368a56e02b647b2be684be1f0a08c89
Vasili Svirski <vasili.svirski@gmail.com>
2020-12-04 17:19:27 +0300

    Count Good Nodes in Binary Tree

---
+++ src/main/java/medium/Solution1448.java
@@ -0,0 +1,17 @@
+package medium;
+
+import common.TreeNode;
+
+public class Solution1448 {
+    public int goodNodes(TreeNode root) {
+        return goodNodes(root, Integer.MIN_VALUE);
+    }
+
+    public int goodNodes(TreeNode root, int ma) {
+        if (root == null) return 0;
+        int res = root.val >= ma ? 1 : 0;
+        res += goodNodes(root.left, Math.max(ma, root.val));
+        res += goodNodes(root.right, Math.max(ma, root.val));
+        return res;
+    }
+}
